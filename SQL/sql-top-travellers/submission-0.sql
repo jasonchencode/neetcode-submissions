d62a47cf68bc8a02bd58b45ec480deb6e9c9b65d
Jason Chen <jasonchenn67@gmail.com>
2026-09-12 17:10:51 -0400

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,6 @@
+-- Write your query below
+SELECT users.name, COALESCE(SUM(rides.distance), 0) AS travelled_distance FROM users
+LEFT JOIN rides
+ON users.id=rides.user_id
+GROUP BY users.name
+ORDER BY travelled_distance DESC, name ASC;
